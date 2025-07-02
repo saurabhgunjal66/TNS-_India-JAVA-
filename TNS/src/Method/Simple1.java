f6d0dcd4b52a2b7a66a8aa382cfc688f606f96ec
Saurabh Gunjal <saurabhgunjal66@gmail.com>
2025-07-02 11:31:08 +0530

    method add

---
+++ TNS/src/Method/Simple1.java
@@ -0,0 +1,12 @@
+package Method;
+
+public class Simple1 {
+	int a;
+	int b;
+	public void add(int a,int b) {
+		this.a=a;
+		this.b=b;
+		int c=a+b;
+		System.out.println("addition:"+c);
+	}
+}
---
+++ TNS/src/Method/Simplemethod.java
@@ -0,0 +1,8 @@
+package Method;
+
+public class Simplemethod {
+	public static void main(String[] args) {
+		Simple1 s=new Simple1();
+		s.add(5, 6);
+	}
+}
