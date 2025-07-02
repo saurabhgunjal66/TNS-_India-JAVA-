f854bf4be7e92f3a89afc576bdf9ad713333e986
Saurabh Gunjal <saurabhgunjal66@gmail.com>
2025-07-02 10:31:40 +0530

    switch case addd

---
+++ TNS/src/conditionalStatement/Switchcase.java
@@ -0,0 +1,29 @@
+package conditionalStatement;
+import java.util.*;
+public class Switchcase {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter name:");
+		int s=sc.nextInt();
+		switch(s) {
+		case 1:
+			System.out.println("monday"); break;
+		case 2:
+			System.out.println("tuesday");break;
+		case 3:
+			System.out.println("wednesday");break;
+		case 4:
+			System.out.println("thursday");break;
+		case 5:
+			System.out.println("friday");break;
+		case 6:
+			System.out.println("saturday");break;
+		case 7:
+			System.out.println("sunday");break;
+		default:
+			System.out.println("invalid");break;
+		}
+	}
+	
+
+}
