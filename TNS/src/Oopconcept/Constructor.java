f4fa30a0c59696c2208b96a2b45fae341fddbf84
Saurabh Gunjal <saurabhgunjal66@gmail.com>
2025-07-07 10:52:15 +0530

    constrructor

--- TNS/src/Oopconcept/Constructor.java
+++ TNS/src/Oopconcept/Constructor.java
@@ -1,5 +1,15 @@
 package Oopconcept;
 
 public class Constructor {
-
+	//default constructor
+	public Constructor() {
+		System.out.println("default constructor");
+	}
+	
+	//parametraze constructor
+	public Constructor(int a,int b) {
+		int c=a+b;
+		System.out.println("addition:"+c);
+	}
+	
 }
---
+++ TNS/src/Oopconcept/ConstructorCall.java
@@ -0,0 +1,8 @@
+package Oopconcept;
+
+public class ConstructorCall {
+	public static void main(String[] args) {
+		Constructor c=new Constructor();
+		Constructor c1=new Constructor(1,2);
+	}
+}
