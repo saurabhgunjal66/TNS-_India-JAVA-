ecc503211958568f15b04de91f17c87c401c2191
Saurabh Gunjal <saurabhgunjal66@gmail.com>
2025-07-11 10:59:16 +0530

    access Modifier

---
+++ TNS/src/accessModifier/Demo.java
@@ -0,0 +1,21 @@
+package accessModifier;
+
+public class Demo {
+	int varDefault=10;
+	private int varPrivate=20;
+	protected int varProtected =30;
+	public int varpublic=40;
+	
+	private void methodPrivate() {
+		
+	}
+	protected void mrethodprotected() {
+		
+	}
+	public void methodpublic() {
+		
+	}
+	void methoddefault() {
+		
+	}
+}
